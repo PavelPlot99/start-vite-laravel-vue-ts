211794c10d4323b2c69de38edc0da6ecc3a9c42b
Pavel <pavel.plotnikov1999@mail.ru>
2023-04-28 14:55:36 +0700

    FEAT:init

---
+++ resources/js/app.js
@@ -0,0 +1,8 @@
+import './bootstrap';
+import '../css/app.css';
+
+import {createApp} from 'vue'
+
+import App from '../ts/App.vue'
+
+createApp(App).mount("#app")
---
+++ resources/ts/App.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+    <h1>{{count}}</h1>
+</div>
+</template>
+
+<script setup lang="ts">
+import {ref, onMounted} from "vue";
+
+const count = ref<string>("Hello world");
+
+onMounted(() => {
+    console.log('Hello TS VUE VITE LARAVEL')
+})
+</script>
+
+<style scoped>
+
+</style>
